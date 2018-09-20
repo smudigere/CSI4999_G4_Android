333ee7c0b46b33069ff0983e3d1cca1b0cb2be1e
Samartha Mudigere <smudigere@couponwallet.com>
2018-09-20 10:38:04 -0400

    Splash Activity added.

---
+++ app/src/main/java/com/gptm/app/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.gptm.app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Removes notification bar
+
+        handler = new Handler();
+        intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        try {
+
+            setContentView(R.layout.activity_splash);
+
+            // Start timer and launch main activity
+            IntentLauncher launcher = new IntentLauncher();
+            launcher.start();
+        } catch (Error error) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            this.finish();
+        }
+    }
+
+    private class IntentLauncher extends Thread {
+
+        /**
+         * Sleep for some time and than start new activity.
+         */
+        @Override
+        public void run() {
+            try {
+                // Sleeping
+                long SLEEP_TIME = 1;
+                Thread.sleep(SLEEP_TIME *1000);
+            } catch (Exception ignored) {}
+
+            // Start MainActivity
+            SplashActivity.this.startActivity(intent);
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    SplashActivity.this.finish();
+                }
+            }, 500);
+        }
+    }
+}
